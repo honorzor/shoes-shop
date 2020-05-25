fce99e53bbae5e7801ab831338550a58963f9f26
ioann <ioanntrapeznikov@gmail.com>
2020-05-26 01:15:38 +0300

    Add new class

---
+++ src/main/java/model/Cart.java
@@ -0,0 +1,41 @@
+package model;
+
+
+import java.util.List;
+import java.util.Objects;
+
+public class Cart {
+    private List<Item> items;
+
+    public Cart(List<Item> items) {
+        this.items = items;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cart cart = (Cart) o;
+        return Objects.equals(items, cart.items);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(items);
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "items=" + items +
+                '}';
+    }
+}
